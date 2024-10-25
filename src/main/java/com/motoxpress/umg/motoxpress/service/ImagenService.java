bd2e1c9f9d5dd90283f347eacc7e256897cc48da
JDavOM26 <Jdavidolivamuralles@gmail.com>
2024-10-24 21:15:21 -0600

    Se editó el servicio de imagen

--- src/main/java/com/motoxpress/umg/motoxpress/service/ImagenService.java
+++ src/main/java/com/motoxpress/umg/motoxpress/service/ImagenService.java
@@ -1,5 +1,28 @@
-package com.motoxpress.umg.motoxpress.service;
 
-public class ImagenService {
 
-}
+    package com.motoxpress.umg.motoxpress.service;
+
+    import java.util.List;
+    import java.util.Optional;
+    
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service;
+
+import com.motoxpress.umg.motoxpress.model.entity.ImagenEntity;
+import com.motoxpress.umg.motoxpress.model.repository.ImagenRepository;
+
+    
+    @Service
+    public class ImagenService {
+        @Autowired
+        private ImagenRepository repository;
+    
+        public List<ImagenEntity> getDataList() {
+            return (List<ImagenEntity>) repository.findAll();
+        }
+    
+         public ImagenEntity getFindUncle(Long value) {
+            Optional<ImagenEntity> res = repository.findById(value);
+            return res.isPresent() ? res.get() : null;
+        }
+    }
