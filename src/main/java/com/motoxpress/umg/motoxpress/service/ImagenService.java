170b465224051775ab73de4543ddba62bce9fcfe
JDavOM26 <Jdavidolivamuralles@gmail.com>
2024-10-24 21:33:05 -0600

    Update ImagenService.java

--- src/main/java/com/motoxpress/umg/motoxpress/service/ImagenService.java
+++ src/main/java/com/motoxpress/umg/motoxpress/service/ImagenService.java
@@ -1,28 +1,51 @@
 
-
-    package com.motoxpress.umg.motoxpress.service;
-
-    import java.util.List;
-    import java.util.Optional;
     
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Service;
+package com.motoxpress.umg.motoxpress.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.motoxpress.umg.motoxpress.model.entity.ImagenEntity;
 import com.motoxpress.umg.motoxpress.model.repository.ImagenRepository;
 
-    
-    @Service
-    public class ImagenService {
-        @Autowired
-        private ImagenRepository repository;
-    
-        public List<ImagenEntity> getDataList() {
-            return (List<ImagenEntity>) repository.findAll();
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.motoxpress.umg.motoxpress.model.repository.PagoRepository;
+
+public class ImagenService implements ServiceCRUD<ImagenEntity>{
+
+    @Autowired
+    private PagoRepository repository;
+
+    @Override
+    public ImagenEntity createOrUpdate(ImagenEntity value) {
+        if (value.getIdImagen() != null) {
+            ImagenEntity personaEntity = getFindUncle(value.getIdImagen());
+            value.setFechaCreacion(personaEntity.getFechaCreacion());
+            value.setUsuarioCreo(personaEntity.getUsuarioCreo());
         }
-    
-         public ImagenEntity getFindUncle(Long value) {
-            Optional<ImagenEntity> res = repository.findById(value);
-            return res.isPresent() ? res.get() : null;
-        }
+        return repository.save(value);
+
     }
+
+    @Override
+    public List<ImagenEntity> getDataList() {
+        return (List<ImagenEntity>) repository.findAll();
+    }
+
+    @Override
+    public ImagenEntity getFindUncle(Long value) {
+       Optional<ImagenEntity> res = repository.findById(value);
+        return res.isPresent() ? res.get() : null;
+
+    }
+
+    @Override
+    public void deleteFind(ImagenEntity value) {
+        repository.delete(value);
+    }
+
+}
